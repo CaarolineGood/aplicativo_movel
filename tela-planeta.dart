e437302600a4b7097bda4cc4e25f597b7b876c5b
CaarolineGood <carolinegoodc@gmail.com>
2025-02-17 23:24:50 -0300

    Add files via upload

---
+++ tela-planeta.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/controles/controle-planeta.dart';
+import 'package:myapp/modelos/planeta.dart';
+
+class TelaPlaneta extends StatefulWidget {
+  final bool isIncluir;
+  final Planeta planeta;
+  final Function() onFinalizado;
+  const TelaPlaneta({
+    super.key,
+    required this.isIncluir,
+    required this.onFinalizado,
+    required this.planeta,
+  });
+
+  @override
+  State<TelaPlaneta> createState() => _TelaPlanetaState();
+}
+
+class _TelaPlanetaState extends State<TelaPlaneta> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _tamanhoController = TextEditingController();
+  final TextEditingController _distanciaController = TextEditingController();
+  final TextEditingController _apelidoController = TextEditingController();
+
+  final ControlePlaneta _controlePlaneta = ControlePlaneta();
+
+  late Planeta _planeta;
+
+  @override
+  void initState() {
+    _planeta = widget.planeta;
+    _nomeController.text = _planeta.nome;
+    _tamanhoController.text = _planeta.tamanho.toString();
+    _distanciaController.text = _planeta.distancia.toString();
+    _apelidoController.text = _planeta.apelido ?? '';
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nomeController.dispose();
+    _tamanhoController.dispose();
+    _distanciaController.dispose();
+    _apelidoController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _inserirPlaneta() async {
+    await _controlePlaneta.inserirPlaneta(_planeta);
+  }
+
+  Future<void> _alterarPlaneta() async {
+    await _controlePlaneta.alterarPlaneta(_planeta);
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      if (widget.isIncluir) {
+        _inserirPlaneta();
+      } else {
+        _alterarPlaneta();
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Dados do Planeta foram ${widget.isIncluir ? 'Incluídos' : 'Alterados'} com sucesso!',
+          ),
+        ),
+      );
+      Navigator.of(context).pop();
+      widget.onFinalizado();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Cadastrar Planeta'),
+        elevation: 3,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nomeController,
+                  decoration: InputDecoration(labelText: 'Nome'),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty || value.length < 3) {
+                      return 'Por favor, insira um nome (mínimo de 3 caracteres)';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _planeta.nome = value!;
+                  },
+                ),
+                TextFormField(
+                  controller: _tamanhoController,
+                  decoration: InputDecoration(labelText: 'Tamanho (em km)'),
+                  keyboardType: TextInputType.number,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira um tamanho';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Por favor, insira um número válido';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _planeta.tamanho = double.parse(value!);
+                  },
+                ),
+                TextFormField(
+                  controller: _distanciaController,
+                  decoration: InputDecoration(labelText: 'Distância (em km)'),
+                  keyboardType: TextInputType.number,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira uma distância';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Por favor, insira um número válido';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _planeta.distancia = double.parse(value!);
+                  },
+                ),
+                TextFormField(
+                  controller: _apelidoController,
+                  decoration: InputDecoration(labelText: 'Apelido (Opicional)'),
+                  onSaved: (value) {
+                    _planeta.apelido = value;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: Text('Cancelar'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _submitForm,
+                      child: Text('Confirmar'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
